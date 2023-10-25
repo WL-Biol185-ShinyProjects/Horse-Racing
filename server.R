656de5dc89068dbdc91f630ee2b723bdd0cd5d02
Abby <portwooda24@mail.wlu.edu>
2023-10-25 14:57:44 -0400

    tesijng

--- server.R
+++ server.R
@@ -1,15 +1,24 @@
 library(shiny)
 library(tidyverse)
+
 coral <- read.csv("global_bleaching_environmental.csv")
 
 function(input, output) {
   
-  output$Temperature_Mean <- renderPlot({
+  #output$Temperature_Mean <- renderPlot({
+  corals %>% filter(input$Ocean_Name)}
+coral %>%
+  filter(Ocean_Name == input$Ocean_Name) %>%
+  ggplot(coral, aes(Temperature_Mean, Date_Year)) + geom_point()
+
+
+
     
-    plot(coral$Temperature_Mean, coral$Date_Year,
-            main = input$Ocean_Name, 
-            ylab = "Year",
-            xlab = "Sea Surface Temperature")
+    # plot(coral$Temperature_Mean, coral$Date_Year,
+    # main = input$Ocean_Name,
+    # ylab = "Year",
+    # xlab = "Sea Surface Temperature"
     
-  })
-}
+  #})
+# })
+
--- ui.R
+++ ui.R
@@ -3,20 +3,33 @@ library(tidyverse)
 coral <- read.csv("global_bleaching_environmental.csv")
 
 fluidPage(
-  
-  titlePanel("Sea Surface Temperature by Ocean"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("Ocean_Name", "Ocean:",
-                  choices = unique(coral$Ocean_Name)),
-      hr(),
-      helpText("Data from Biological & Chemical Oceanography Data Mangement Office (2022) Global Bleaching and Environmental Data.")
-    ),
-    
-    mainPanel(
-      plotOutput("Temperature_Mean")
-    )
+  titlePanel(title= "Sea Surface Temperature by Ocean")
+)
+sidebarPanel(
+  sliderInput("Ocean Name", "Ocean:",
+              min = 0, max = 310, step=20
+              , value=c(0,310)),
+  mainPanel(
+    plotOutput("Temperature_Mean")
   )
-)  
+)
 
+
+# fluidPage(
+#   
+#   titlePanel("Sea Surface Temperature by Ocean"),
+#   
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput("Ocean_Name", "Ocean:",
+#                   choices = unique(coral$Ocean_Name)),
+#       hr(),
+#       helpText("Data from Biological & Chemical Oceanography Data Mangement Office (2022) Global Bleaching and Environmental Data.")
+# #     ),
+# #     
+# #     mainPanel(
+# #       plotOutput("Temperature_Mean")
+#     )
+#   )
+# )  
+# 
