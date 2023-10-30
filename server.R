d04996e749d5b0c165b6555ff86a9c45ca64678a
Abby <portwooda24@mail.wlu.edu>
2023-10-30 16:43:26 -0400

    creating dashboard

--- server.R
+++ server.R
@@ -7,14 +7,9 @@ function(input, output) {
   
   output$Temperature_Mean <- renderPlot({
 
-    global_bleaching_environmental %>%
+    coral %>%
       filter(Ocean_Name == input$Ocean_Name) %>%
       ggplot(aes(Date, Temperature_Mean)) + geom_point(color = "blue")
-    
-    #plot(coral$Temperature_Mean, coral$Date_Year,
-            #main = input$Ocean_Name, 
-            #ylab = "Year",
-            #xlab = "Sea Surface Temperature")
 
     
   })
--- ui.R
+++ ui.R
@@ -2,23 +2,55 @@ library(shiny)
 library(tidyverse)
 coral <- read.csv("global_bleaching_environmental.csv")
 
-fluidPage(
-  
+library(shinydashboard)
 
-  titlePanel("Sea Surface Temperature by Ocean"),
-)
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("Ocean_Name", "Ocean:",
+droppy1 <- fluidPage(
+  
+  
+    titlePanel("Sea Surface Temperature by Ocean"),
+  
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("Ocean_Name", "Ocean:",
                   choices = unique(coral$Ocean_Name)),
-      hr(),
-      helpText("Data from Biological & Chemical Oceanography Data Mangement Office (2022) Global Bleaching and Environmental Data.")
-    ),
+        hr(),
+        helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
+      ),
     
-    mainPanel(
-      plotOutput("Temperature_Mean")
+      mainPanel(
+        plotOutput("Temperature_Mean")
+        )
+      )
+    )
+
+
+sidebar <- dashboardSidebar( 
+  sidebarMenu("SeaSurfaceTemperaturebyOcean", tabName = "SeaSurfaceTemperaturebyOcean", icon = icon("SeaSurfaceTemperaturebyOcean")),
+    menuItem("SeaSurfaceTemperaturebyOcean", icon = icon("th"), tabName = "SeaSurfaceTemperaturebyOcean"
+           )
+  )
+
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "SeaSurfaceTemperaturebyOcean", droppy1
+  ),
+    
+    tabItem(tabName = "SeaSurfaceTemperatureByOcean", droppy1
     )
   )
+)
+
+dashboardPage(
+  dashboardHeader(title = "Coral"),
+  sidebar, 
+  body
+  )
+  
+    
+# https://rstudio.github.io/shinydashboard/structure.html
+
+
 
 
 
