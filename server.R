b775b75ec17923cad9c82933d98aa887d56f056f
Abby <portwooda24@mail.wlu.edu>
2023-11-27 14:53:17 -0500

    getting my push back

--- server.R
+++ server.R
@@ -1,52 +1,92 @@
 library(shiny)
 library(tidyverse)
+library(plotly)
 
 coral <- read.csv("global_bleaching_environmental.csv", na.strings = "nd")
 
 Coral_Ordered1 <- coral %>% group_by(Country_Name, Date_Year) %>% summarise(aveTemp = mean(Temperature_Mean, na.rm = TRUE))
-Coral_Ordered2 <- coral %>% group_by(Ecoregion_Name, Date_Year) %>% summarise(aveBleach = mean(Percent_Bleaching, na.rm = TRUE))
-Coral_Ordered3 <- coral %>% group_by(Ocean_Name, Date_Year) %>% summarise(aveTemp = mean(Temperature_Mean, na.rm = TRUE))
-Coral_Ordered4 <- coral %>% group_by(Ocean_Name, Date_Year) %>% summarise(aveBleach = mean(Percent_Bleaching, na.rm = TRUE))
-Coral_Ordered5 <- coral %>% group_by(Ecoregion_Name, Date_Year) %>% summarise(aveTemp = mean(Temperature_Mean, na.rm = TRUE))
 
 function(input, output) {
   
   output$Temperature_Mean <- renderPlot({
-
-    Coral_Ordered3 %>%
+    
+    coral %>%
       filter(Ocean_Name == input$Ocean_Name) %>%
-      ggplot(aes(Date_Year, aveTemp)) + geom_point(color = "blue") 
-
+      ggplot(aes(Date, Temperature_Mean)) + geom_point(color = "blue")
+    
   })
   
   output$Percent_Bleaching <- renderPlot({
     
-    Coral_Ordered4 %>%
+    coral %>%
       filter(Ocean_Name == input$Ocean_Name1) %>%
-      ggplot(aes(Date_Year, aveBleach)) + geom_point(color = "blue") 
+      ggplot(aes(Date, Percent_Bleaching)) + geom_point(color = "blue")
   })
   
   output$Temperature_Mean1 <- renderPlot({
     
-    Coral_Ordered5 %>%
+    coral %>%
       filter(Ecoregion_Name == input$Ecoregion_Name) %>%
-      ggplot(aes(Date_Year, aveTemp)) + geom_point(color = "blue")
+      ggplot(aes(Date, Temperature_Mean)) + geom_point(color = "blue")
   })
   
   output$Percent_Bleaching1 <- renderPlot({
     
-    Coral_Ordered2 %>%
+    coral %>%
       filter(Ecoregion_Name == input$Ecoregion_Name1) %>%
-      ggplot(aes(Date_Year, aveBleach)) + geom_point(color = "blue") 
+      ggplot(aes(Date, Percent_Bleaching)) + geom_point(color = "blue")
   })
-
+  
   output$Temperature_Mean2 <- renderPlot({
     
     Coral_Ordered1 %>%
       filter(Date_Year == input$Date_Year) %>%
-      ggplot(aes(Country_Name, aveTemp)) + geom_bar(stat = 'identity', position = "jitter") + scale_x_discrete(guide = guide_axis(angle = 90)) + NULL
+      ggplot(aes(Country_Name, aveTemp)) + geom_bar(stat = 'identity', position = "jitter")
+  })
+  
+  output$zoomableDensityPlot <- renderPlotly({
+    p <- ggplot(coral, aes(x = Percent_Bleaching, fill = as.factor(Date_Year))) +
+      geom_density(alpha = 0.5) +
+      labs(title = "Density Plot of Percent Bleaching by Year",
+           x = "Percent Bleaching",
+           y = "Density") +
+      theme_minimal()
+    
+    ggplotly(p) %>%
+      layout(dragmode = "select")  # Allow for box selection
+  })
+  
+  output$map <- renderLeaflet({
+    
+    unique_sites <- coral %>% 
+      distinct(Site_Name, .keep_all = TRUE)  # Keep only the first occurrence of each unique Site_Name
+    
+    leaflet(data = unique_sites) %>%
+      addTiles() %>%
+      addMarkers(~Longitude_Degrees, ~Latitude_Degrees, popup = ~Site_Name)
+  })
+  
+  output$location_info <- renderPrint({
+    click <- input$map_marker_click
+    if (!is.null(click)) {
+      selected_row <- coral[coral$Site_Name == click$id, ]
+      selected_info <- paste(
+        "Site Name: ", selected_row$Site_Name,
+        "\nOcean Name: ", selected_row$Ocean_Name,
+        "\nReef ID: ", selected_row$Reef_ID,
+        "\nEcoregion: ", selected_row$Ecoregion_Name,
+        "\nCountry: ", selected_row$Country_Name,
+        "\nLatitude: ", selected_row$Latitude_Degrees,
+        "\nLongitude: ", selected_row$Longitude_Degrees,
+        "\nPercent Bleaching: ", selected_row$Percent_Bleaching,
+        "\nTemperature (Kelvin): ", selected_row$Temperature_Kelvin
+        # Add other columns as needed
+      )
+      return(selected_info)
+    }
   })
 }
+
 
 
 
--- ui.R
+++ ui.R
@@ -1,28 +1,40 @@
 library(shiny)
 library(tidyverse)
+library(plotly)
+library(shinydashboard)
+library(leaflet)
+
 coral <- read.csv("global_bleaching_environmental.csv", na.strings = "nd")
 
-library(shinydashboard)
-
-
 droppy1 <- fluidPage(
-  box(title = "Input", status = "warning", solidHeader = TRUE, width = 4,
-        selectInput("Ocean_Name", "Ocean:",
+  titlePanel("Sea Surface Temperature by Ocean"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("Ocean_Name", "Ocean:",
                   choices = unique(coral$Ocean_Name)),
-        hr(),
-        helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
-      ),
-    
-  box(title = "Sea Surface Temperature by Ocean", status = "primary", solidHeader = TRUE, width = 8, plotOutput("Temperature_Mean", height = 500)
-        )
-      )
-    
+      hr(),
+      helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
+    ),
+    mainPanel(
+      plotOutput("Temperature_Mean"),
+      helpText("This plot displays the sea surface temperature variation over time for the selected ocean.")
+    )
+  )
+)
+box(title = "Input", status = "warning", solidHeader = TRUE, width = 4,
+    selectInput("Ocean_Name", "Ocean:",
+                choices = unique(coral$Ocean_Name)),
+    hr(),
+    helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
+)
+
+box(title = "Sea Surface Temperature by Ocean", status = "primary", solidHeader = TRUE, width = 8, plotOutput("Temperature_Mean", height = 500)
+)
+
+
 
 droppy2 <- fluidPage(
-  
-  
   titlePanel("Percent Bleaching by Ocean"),
-  
   sidebarLayout(
     sidebarPanel(
       selectInput("Ocean_Name1", "Ocean:",
@@ -30,17 +42,15 @@ droppy2 <- fluidPage(
       hr(),
       helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
     ),
-    
     mainPanel(
-      plotOutput("Percent_Bleaching")
+      plotOutput("Percent_Bleaching"),
+      helpText("This plot shows the percentage of bleaching over time for the selected ocean.")
     )
   )
 )
 
 droppy3 <- fluidPage(
-  
   titlePanel("Sea Surface Temperature by Ecoregion"),
-  
   sidebarLayout(
     sidebarPanel(
       selectInput("Ecoregion_Name", "Ecoregion:",
@@ -48,56 +58,98 @@ droppy3 <- fluidPage(
       hr(),
       helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
     ),
-    
     mainPanel(
-      plotOutput("Temperature_Mean1")
+      plotOutput("Temperature_Mean1"),
+      helpText("This plot displays the sea surface temperature variation over time for the selected ecoregion.")
     )
   )
 )
 
 droppy4 <- fluidPage(
-  box(title = "Percent Bleaching by Ecoregion", status = "warning", solidHeader = TRUE, background = NULL, width = 4,
+  titlePanel("Percent Bleaching by Ecoregion"),
+  sidebarLayout(
+    sidebarPanel(
       selectInput("Ecoregion_Name1", "Ecoregion:",
                   choices = unique(coral$Ecoregion_Name)),
       hr(),
       helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
     ),
-  
-    
-  box(title = "Percent Bleaching", status = "primary", solidHeader = TRUE, background = NULL, width = 8, plotOutput("Percent_Bleaching1", height = 500)
+    mainPanel(
+      plotOutput("Percent_Bleaching1"),
+      helpText("This plot shows the percentage of bleaching over time for the selected ecoregion.")
+    )
   )
 )
-  
-
 
 droppy5 <- fluidPage(
-  box(
-    title = "Temperature Mean by Country", status = "primary", solidHeader = TRUE, collapsible = TRUE, width = 8, 
-    plotOutput("Temperature_Mean2", height = 750)
+  titlePanel("Temperature Mean by Country"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("Date_Year", "Year:",
+                  min = 1980, max = 2020, value = 2000, step = NULL),
+      hr(),
+      helpText("Data from Biological & Chemical Oceanography Data Management Office (2022) Global Bleaching and Environmental Data.")
     ),
-  box(
-    title = "Inputs", status = "warning", solidHeader = TRUE, width = 4,
-  sliderInput("Date_Year", "Year:",
-              min = 1980, max = 2020, value = 2000, step =NULL
-              )
-  
-      )
+    mainPanel(
+      plotOutput("Temperature_Mean2"),
+      helpText("This plot displays the mean temperature by country for the selected year.")
+    )
+  )
 )
+
+droppy6 <- fluidPage(
+  titlePanel("Zoomable Density Plot of Percent Bleaching"),
+  mainPanel(
+    plotlyOutput("zoomableDensityPlot"),
+    helpText("This plot represents the density distribution of percent bleaching over different years with zooming capabilities.")
+  )
+)
+
+mappy1 <- fluidPage(
+  leafletOutput("map"),
+  verbatimTextOutput("location_info")
+)
+
+
+# textbox <- fluidPage(
+#   p("Here we will put an introduction for our website.")
+# )
 
 textbox <- fluidPage(
-  
-  p("here we will put an introduction for our website")
-)
-
-
-sidebar <- dashboardSidebar(
-  sidebarMenu("Contents",
-              menuItem("Home", tabName = "Home", icon = icon("house")),
-              menuItem("SeaSurfaceTemperaturebyOcean", tabName = "SeaSurfaceTemperaturebyOcean", icon = icon("temperature-three-quarters")),    
-              menuItem("PercentBleachingbyOcean", tabName = "PercentBleachingbyOcean", icon = icon("droplet")),
-              menuItem("SeaSurfaceTemperaturebyEcoregion", tabName = "SeaSurfaceTemperaturebyEcoregion", icon = icon("droplet")),
-              menuItem("PercentBleachingbyEcoregion", tabName = "PercentBleachingbyEcoregion", icon = icon("droplet")),
-              menuItem("TemperatureMeanbyCountry", tabName = "TemperatureMeanbyCountry", icon = icon("droplet"))
+  titlePanel("Welcome to the Coral Reefs Dashboard"),
+  #sidebarLayout(
+  #sidebarPanel(width = 3, ""),
+  mainPanel(
+    fluidRow(
+      column(
+        width = 12,
+        tags$img(src = "https://rstudioworkbench.wlu.edu/s/fdf7f96ca1b478d511567/files/Horse-Racing/coralbanner.png", width = "100%")
+      )
+    ),
+    fluidRow(
+      column(
+        width = 12,
+        box(
+          title = "Introduction",
+          status = "primary", 
+          solidHeader = TRUE,
+          width = 12,
+          p("This dashboard provides insights into ocean temperatures, coral bleaching, and more...")
+        )
+      )
+    ),
+    fluidRow(
+      column(
+        width = 12,
+        box(
+          title = "Dashboard Features",
+          status = "info",
+          solidHeader = TRUE,
+          width = 12,
+          "Here you can find information on different aspects related to coral reefs and ocean data."
+        )
+      )
+    )
   )
 )
 
@@ -105,25 +157,36 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "Home", textbox),
-    tabItem(tabName = "SeaSurfaceTemperaturebyOcean", droppy1
-    ),
+    tabItem(tabName = "SeaSurfaceTemperaturebyOcean", droppy1),
     tabItem(tabName = "PercentBleachingbyOcean", droppy2),
     tabItem(tabName = "SeaSurfaceTemperaturebyEcoregion", droppy3),
     tabItem(tabName = "PercentBleachingbyEcoregion", droppy4),
-    tabItem(tabName = "TemperatureMeanbyCountry", droppy5)
+    tabItem(tabName = "TemperatureMeanbyCountry", droppy5),
+    tabItem(tabName = "densityPlot", droppy6),
+    tabItem(tabName = "map", mappy1)
+    
   )
-)     
-
-
-
+)
 
 dashboardPage(skin = "purple",
-  dashboardHeader(title = "Coral"),
-  sidebar, 
-  body
-  )
-  
-    
+              dashboardHeader(title = "Coral"),
+              dashboardSidebar(
+                sidebarMenu("Contents",
+                            menuItem("Home", tabName = "Home", icon = icon("house")),
+                            menuItem("SeaSurfaceTemperaturebyOcean", tabName = "SeaSurfaceTemperaturebyOcean", icon = icon("temperature-three-quarters")),    
+                            menuItem("PercentBleachingbyOcean", tabName = "PercentBleachingbyOcean", icon = icon("droplet")),
+                            menuItem("SeaSurfaceTemperaturebyEcoregion", tabName = "SeaSurfaceTemperaturebyEcoregion", icon = icon("droplet")),
+                            menuItem("PercentBleachingbyEcoregion", tabName = "PercentBleachingbyEcoregion", icon = icon("droplet")),
+                            menuItem("TemperatureMeanbyCountry", tabName = "TemperatureMeanbyCountry", icon = icon("droplet")),
+                            menuItem("DensityPlot", tabName = "densityPlot", icon = icon("droplet")),
+                            menuItem("Map", tabName = "map", icon = icon("droplet"))
+                            
+                )
+              ),
+              body
+)
+
+
 # https://rstudio.github.io/shinydashboard/structure.html
 
 #Coral_Ordered1 <- coral %>% group_by(Country_Name, Date_Year) %>% summarise(aveTemp = mean(Temperature_Mean, na.rm = TRUE))
